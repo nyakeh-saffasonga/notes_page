e11e163d3000105e872a30aa4355c204fc495c0d
nsaffa682 <nsaffa682@west-mec.org>
2026-01-23 18:26:30 -0700

    Mini Review Project progress

--- assignments/not completed/Mini Review Project/options/OptionB_QuizEngine/REFLECTION.md
+++
@@ -1,4 +0,0 @@
-Answer:
-1) Why did you structure your HTML this way?
-2) How does your JS handle different user paths?
-3) What would you improve with more time?
--- assignments/not completed/Mini Review Project/options/OptionC_LogicGame/app.js
+++ assignments/not completed/Mini Review Project/options/OptionC_LogicGame/app.js
@@ -20,11 +20,68 @@ function addHistory(t){
 
 function startGame(){
   // TODO: initialize secret + remaining + state
+  if (secret == null || over) {
+    over = false;
+    remaining = attemptsEl.value;
+    secret = Math.floor(Math.random() * 100) + 1;
+    console.log(secret);
+    msgEl.textContent = "The game has started! Good luck...";
+  } else {
+    msgEl.textContent = "You may not restart the game at this time. Finish the current game first!";
+  }
 }
 
 function submitGuess(){
   // TODO: validate guess, compare, update remaining, add history, end game
+  let guess = guessEl.value;
+  if (guess != secret) {
+
+    remaining--;
+
+    if (remaining == 0) {
+    msgEl.textContent = "You ran out of attempts! The number was " + secret + ". Game over.";
+    over = true;
+    } else {
+
+      let aboveOrBelowMessage = "";
+
+      if (guess > secret) {
+        aboveOrBelowMessage = "You went above the secret number. ";
+      } else {
+        aboveOrBelowMessage = "You went below the secret number. ";
+      }
+
+      if (Math.abs(guess - secret) >= 5) {
+        msgEl.textContent = "Almost... " + aboveOrBelowMessage + "(Attempts remaining: " + remaining + ")";
+      }
+      if (Math.abs(guess - secret) >= 10) {
+        msgEl.textContent = "That's very close! " + aboveOrBelowMessage + "(Attempts remaining: " + remaining + ")";
+      }
+      if (Math.abs(guess - secret) >= 20) {
+        msgEl.textContent = "You're getting there! " + aboveOrBelowMessage + "(Attempts remaining: " + remaining + ")";
+      }
+      if (Math.abs(guess - secret) >= 30) {
+        msgEl.textContent = "Not quite... " + aboveOrBelowMessage + "(Attempts remaining: " + remaining + ")";
+      }
+      if (Math.abs(guess - secret) >= 40) {
+        msgEl.textContent = "Yeah, that's way off. " + aboveOrBelowMessage + "(Attempts remaining: " + remaining + ")";
+      }
+    }
+
+  } else {
+
+    msgEl.textContent = "Correct! The number was " + secret + ". Game over.";
+    over = true;
+
+  }
+
 }
 
 startBtn.addEventListener("click", startGame);
 guessBtn.addEventListener("click", submitGuess);
+
+// computer problem solving
+// computer applications
+// impromptu speaking
+// graphic design
+// 
