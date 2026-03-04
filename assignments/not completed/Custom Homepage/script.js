69701c42ea2308db72f4a3130ce264724d162fbc
nsaffa682 <nsaffa682@west-mec.org>
2026-03-03 18:28:03 -0700

    site progress

--- assignments/not completed/Custom Homepage/script.js
+++ assignments/not completed/Custom Homepage/script.js
@@ -1,17 +1,50 @@
-// typewriter animation of the page
+// "cyber's corner" typewriter animation
 
 let titleElement = document.querySelector("h1");
 let fullText = titleElement.innerText; // stores the original text
-titleElement.innerText = ""; // clear it to start the animation
+titleElement.innerText = ""; // clears it to start the animation
 let index = 0;
 
-let titleAnimation = setInterval(() => {
+setInterval(() => {
     if (index < fullText.length) {
         titleElement.innerText = fullText.slice(0, index + 1) + "|";
         index++;
     } else {
-        // Remove the cursor and stop
+        // where it stops
         titleElement.innerText = fullText;
         clearInterval(titleAnimation);
     }
 }, 100);
+
+
+
+
+
+// time updater
+
+let timeElement = document.getElementById("time");
+let currentDateElement = document.getElementById("date")
+let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+function updateTime() {
+
+    let date = new Date();
+
+    currentDateElement.innerText = `${days[date.getDay() - 1]}, ${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`; // current day
+
+    timeElement.innerText = `${date.toLocaleString('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit' })}`; // live clock
+
+}
+
+updateTime();
+setInterval(updateTime , 1000);
+
+
+
+
+
+// welcoming message
+
+
+
