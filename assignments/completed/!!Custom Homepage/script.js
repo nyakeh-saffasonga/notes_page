8d7cce723f837723b48ee0bd4b0ea20385cbc4e2
nsaffa682 <nsaffa682@west-mec.org>
2026-03-09 18:25:12 -0700

    m

--- assignments/completed/!!Custom Homepage/script.js
+++ assignments/completed/!!Custom Homepage/script.js
@@ -23,9 +23,9 @@ setInterval(() => {
 // time updater
 
 let timeElement = document.getElementById("time");
-let currentDateElement = document.getElementById("date")
-let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+let currentDateElement = document.getElementById("date");
+let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 function updateTime() {
 
