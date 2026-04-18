9b4575b1214f54f91dfd14041e7b7bf4bfe27847
nsaffa682 <nsaffa682@west-mec.org>
2026-04-17 18:26:22 -0700

    b

--- assignments/not completed/Personal Portfolio Website/script.js
+++ assignments/not completed/Personal Portfolio Website/script.js
@@ -1,13 +1,5 @@
-function drawHexagon(ctx, x, y, r) {
-    const angle = (2 * Math.PI) / 6;
-    ctx.beginPath();
-    for (let i = 0; i < 6; i++) {
-    ctx.lineTo(x + r * Math.cos(angle * i), y + r * Math.sin(angle * i));
-    }
-    ctx.closePath();
-    ctx.stroke();
-}
-
 let canvas = document.getElementById("hexagon");
-let hexagonImage = document.getElementById("hexagonSVG")
+let hexagonImage = document.getElementById("hexagonImage")
+
+hexagonImage.addEventListener
 
